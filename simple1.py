df452a1e9083fe1b8e15115a7085844454a406bd
jclm-rt <juliocesarlapaca@gmail.com>
2021-07-12 05:41:29 -0400

    cambio de switch no hard coded

--- simple1.py
+++ simple1.py
@@ -13,13 +13,12 @@ nr = InitNornir(config_file="config.yaml")
 
 nr.inventory.defaults.username = os.getenv("USERNAME_NETADMIN")
 nr.inventory.defaults.password = os.getenv("PASSWORD_NETADMIN")
-#nr.inventory.defaults.port = 22
 
 def nugget_test(task):
     task.run(napalm_get, getters=["get_facts"])
 
 #results = nr.run(task=nugget_test)
-devices = nr.filter(asset_tag = "01-SW03-R1P2")
+devices = nr.filter(asset_tag = "01-SW02-R1P2")
 result = devices.run(task=nugget_test)
 print_result(result)
 failure = nr.data.failed_hosts
